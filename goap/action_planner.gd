74ee0b8d54d1bba02f2b104289d9b6e846e0c875
Robert33r33 <robertrittner@t-online.de>
2023-04-29 12:17:34 +0200

    first attempt action planner

--- goap/action_planner.gd
+++ goap/action_planner.gd
@@ -23,8 +23,12 @@ func set_actions(actions: Array):
 func get_plan(goal: GoapGoal, blackboard = {}) -> Array:
 	print("Goal: %s" % goal.get_clazz())
 	WorldState.console_message("Goal: %s" % goal.get_clazz())
-	# TODO: fill with logic
-	return []
+	#get the desired goal and find the best plan to achieve it
+	var desired_state = goal.get_desired_state().duplicate()
+	#if there is no desired state we can't find a plan to achieve it so retun empty array
+	if desired_state.is_empty():
+		return []
+	return _find_best_plan(goal, desired_state, blackboard)
 
 
 #
@@ -32,8 +36,17 @@ func get_plan(goal: GoapGoal, blackboard = {}) -> Array:
 # _get_cheapest_plan to calculate the best overall plan.
 #
 func _find_best_plan(goal, desired_state, blackboard):
-	# TODO: fill with logic
-	return []
+	#object can be changed in function (like a pointer), so you can only return bool in _build_plans
+
+	var tree = {
+			"children": [],
+			"desired_states": desired_state
+		}
+	tree.graph.add_node("root")
+	if _build_plans(tree, blackboard):
+		return _get_cheapest_plan(_transform_tree_into_array(tree, blackboard))
+	else:
+		return []
 
 
 #
@@ -41,8 +54,13 @@ func _find_best_plan(goal, desired_state, blackboard):
 # Returns actions included in the cheapest plan.
 #
 func _get_cheapest_plan(plans):
-	# TODO: fill with logic
-	return []
+	#cycle through all plans and compare costs
+	var cheapest_plan = plans[0]
+	for i in range(1, plans.size()):
+		_print_plan(plans[i])
+		if plans[i].cost < cheapest_plan.cost:
+			cheapest_plan = plans[i]
+	return cheapest_plan
 
 
 #
@@ -61,8 +79,27 @@ func _get_cheapest_plan(plans):
 # circular dependencies. This is easy to implement though.
 #
 func _build_plans(step, blackboard):
-	# TODO: fill with logic
-	return false
+	#
+	var valid_plan = false
+	#go through all actions and check if they have the d esired effect
+	for desired_state in step.desired_states:
+		for action in _actions:
+			for effect in action.get_effects():
+				if desired_state == effect:
+					#for actions that have the desired effect repeat
+					var preconditions = action.get_preconditions()
+					if preconditions:
+						for precondition in action.get_preconditions():
+							var next_step ={
+								"action": action,
+								"children": [],
+								"desired_states": action.get_preconditions()
+							}
+							step.children.add(_build_plans(next_step, blackboard))
+					# if there are not preconditions it is the end of the plan
+					else:
+						valid_plan = true
+	return valid_plan
 
 #
 # Transforms graph with actions into list of actions and calculates
@@ -71,8 +108,16 @@ func _build_plans(step, blackboard):
 # Returns list of plans.
 #
 func _transform_tree_into_array(p, blackboard):
-	# TODO: fill with logic
-	return []
+	var plans = []
+	
+	if not p.children:
+		plans.append([])
+	else:
+		for child in p.children:
+			var sub_plans = _transform_tree_into_array(child, blackboard)
+			for sub_plan in sub_plans:
+				plans.append([child.action] + sub_plan)
+	return plans
 
 
 #
