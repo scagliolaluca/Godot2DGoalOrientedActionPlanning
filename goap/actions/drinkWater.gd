27a6087c07e907883288ab75d54b50cccf9c9be3
Sycephoa <fr2202fr@gmail.com>
2023-05-08 16:16:40 +0200

    attempt at making a timer.

--- goap/actions/drinkWater.gd
+++ goap/actions/drinkWater.gd
@@ -2,6 +2,8 @@ extends GoapAction
 
 class_name DrinkAction
 
+const Water = preload("res://scenes/water.tscn")
+
 func get_clazz(): return "DrinkAction"
 
 func get_preconditions():
@@ -14,13 +16,14 @@ func get_cost(_blackboard) -> int:
 	return 12
 
 func perform(actor,delta):
-	var water = WorldState.get_closest_element("water_spot", actor)
-	if water:
-		if water.position.distance_to(actor.position) < 2: #just like in firepit
-			#is instant for now. better would be like tree.
+	var nearestwater = WorldState.get_closest_element("water_spot", actor)
+	if nearestwater:
+		if nearestwater.position.distance_to(actor.position) < 2: #just like in firepit
+			var water = Water.instantiate()
+			actor.get_parent().add_child(water)
 			WorldState.set_state("has_drunk",true)
 			return true
 		else:#has to move
-			var moveDirection = water.position - actor.position
+			var moveDirection = nearestwater.position - actor.position
 			actor.move_to(moveDirection, delta)
 	return false
